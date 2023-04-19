9c7ffe9999d797f8741aa265b2a4308085a2fa83
LeslieAine <aineleslie@gmail.com>
2023-04-19 18:12:40 +0300

    fix linters

--- person.rb
+++ person.rb
@@ -32,7 +32,7 @@ end
 
 person = Person.new(22, 'maximilianus')
 puts person.correct_name
-capitalizedPerson = CapitalizeDecorator.new(person)
-puts capitalizedPerson.correct_name
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-puts capitalizedTrimmedPerson.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
