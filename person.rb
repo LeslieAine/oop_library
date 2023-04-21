8bdaa8ac634618e76a5810afa76ca999dfa81fda
LeslieAine <aineleslie@gmail.com>
2023-04-21 15:32:31 +0300

    Add basic UI to app

--- person.rb
+++ person.rb
@@ -36,9 +36,3 @@ class Person < Nameable
   end
 end
 
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
