20f936a65a144e0c042d655f0d0890ff9be9209c
LeslieAine <aineleslie@gmail.com>
2023-04-26 11:02:49 +0300

    fix linters

--- Save/storage.rb
+++ Save/storage.rb
@@ -1,17 +1,17 @@
 class Storage
-    def fetch
-      raise Error 'Not implemented'
-    end
-  
-    def save(_item)
-      raise Error 'Not implemented'
-    end
-  
-    def serialize(_item)
-      raise Error 'Not implemented'
-    end
-  
-    def deserialize(_item)
-      raise Error 'Not implemented'
-    end
+  def fetch
+    raise Error 'Not implemented'
   end
+
+  def save(_item)
+    raise Error 'Not implemented'
+  end
+
+  def serialize(_item)
+    raise Error 'Not implemented'
+  end
+
+  def deserialize(_item)
+    raise Error 'Not implemented'
+  end
+end
