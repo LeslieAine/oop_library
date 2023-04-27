420247102cc89c6e410d2ff34d00e62ec824813e
LeslieAine <aineleslie@gmail.com>
2023-04-27 23:20:35 +0300

    fix errors

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -44,20 +44,14 @@ describe Person do
     end
   end
 
-  describe '#to_h' do
-            let(:person) { Person.new(1, 18, name: 'Test Name', parent_permission: true) }
+  it 'should add a person to the rentals' do
+    book = Book.new("Title", "Author")
+    person = Person.new(24, "Bob")
+    rental = Rental.new("2023-04-27", book, person)
+    
+    person.add_rental(rental)
+    
+    expect(person.rentals).to include(rental)
+  end
 
-            it 'returns a hash with person details and rental details' do
-            rental = instance_double('Rental', to_h: { book_id: 1, person_id: 1, date: '2023-04-25' })
-            person.instance_variable_set(:@rentals, [rental])
-            expect(person.to_h).to eq({
-                                        id: 1,
-                                        name: 'Test Name',
-                                        age: 18,
-                                        parent_permission: true,
-                                        rentals: [rental.to_h]
-                                        })
-            end
-        end
-    end
 end
